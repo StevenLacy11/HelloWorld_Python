cc4391ac04566f3d47f4264191ded4415d1889fa
Steven Lacy <steven.lacy11@gmail.com>
2022-02-21 12:56:35 -0600

    python variables

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,22 @@
 
 print("Hello world!")
 
+# variables
+
+# age = 20
+sentence = "My name is Steven"
+
+print sentence
+
+# mike = 23
+# sarah = 21
+# bob = 32
+
+mike, sarah, bob = 23, 21, 32
+
+print mike
+
+name, age = "Steven", 26
+
+print name
+print age
